0c7b1331df270752f91c39e3c5f371ba35774216
hohserg <hohserg1@yandex.ru>
2020-10-07 23:34:54 +0300

    Remove test classes

--- src/main/java/hohserg/elegant/networking/test/Test2.java
+++
@@ -1,8 +0,0 @@
-package hohserg.elegant.networking.test;
-
-public class Test2 {
-    public static void main(String[] args) {
-        //System.out.println(new Test());
-        //System.out.println(new Test().getA());
-    }
-}
