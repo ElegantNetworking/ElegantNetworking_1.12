f536d43f0c582d088fac72deb00d4d9510f63ef3
hohserg <hohserg1@yandex.ru>
2021-07-07 15:56:50 +0300

    Use ServiceLoader

--- src/main/java/hohserg/elegant/networking/impl/Main.java
+++ src/main/java/hohserg/elegant/networking/impl/Main.java
@@ -1,18 +1,15 @@
 package hohserg.elegant.networking.impl;
 
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 @Mod(modid = "elegant_networking", name = "ElegantNetworking")
 public class Main {
 
-    public static Logger log;
+    private static Logger log;
     public static Config config;
 
     @Mod.EventHandler
@@ -23,15 +20,7 @@ public class Main {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        Init.registerAllPackets(
-                Loader.instance().getActiveModList()
-                        .stream()
-                        .map(mod -> new Init.ModInfo(mod.getModId(), mod.getSource()))
-                        .collect(Collectors.toList()),
-                log::info,
-                log::error,
-                Network.getNetwork()::registerChannel
-        );
+        Init.initPackets(log::info, log::error, Network.getNetwork()::registerChannel);
     }
 
     @Mod.EventHandler
